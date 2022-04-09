1dc063d8d87d6ae16cce96f72d2cbbf7d1dd236f
Matthias Spiller <matthias.spiller@fari-software.de>
2022-04-09 16:24:09 +0200

    Remove logging

--- charger/amtron_prof.go
+++ charger/amtron_prof.go
@@ -43,18 +43,14 @@ func NewAmtronProfessionalFromConfig(other map[string]interface{}) (api.Charger,
 	return NewAmtronProfessional(cc.URI, cc.Device, cc.Comset, cc.Baudrate, cc.ID)
 }
 
-// NewAmtronProfessional creates Amtron charger
+// NewAmtronProfessional creates AmtronProfessional charger
 func NewAmtronProfessional(uri, device, comset string, baudrate int, slaveID uint8) (api.Charger, error) {
 	conn, err := modbus.NewConnection(uri, device, comset, baudrate, modbus.Tcp, slaveID)
 	if err != nil {
 		return nil, err
 	}
 
-	log := util.NewLogger("amtron_prof")
-	conn.Logger(log.TRACE)
-
 	wb := &AmtronProfessional{
-		log:     log,
 		conn:    conn,
 		current: 6,
 	}
@@ -64,7 +60,6 @@ func NewAmtronProfessional(uri, device, comset string, baudrate int, slaveID uin
 
 // Status implements the api.Charger interface
 func (wb *AmtronProfessional) Status() (api.ChargeStatus, error) {
-	wb.log.DEBUG.Println("Status")
 	b, err := wb.conn.ReadHoldingRegisters(amtronProfRegStatus, 1)
 	if err != nil {
 		return api.StatusNone, err
@@ -90,7 +85,6 @@ func (wb *AmtronProfessional) Status() (api.ChargeStatus, error) {
 
 // Enabled implements the api.Charger interface
 func (wb *AmtronProfessional) Enabled() (bool, error) {
-	wb.log.DEBUG.Println("Enabled")
 	b, err := wb.conn.ReadHoldingRegisters(amtronProfRegAmpsConfig, 1)
 	if err != nil {
 		return false, err
@@ -98,14 +92,11 @@ func (wb *AmtronProfessional) Enabled() (bool, error) {
 
 	var value uint16 = binary.BigEndian.Uint16(b)
 
-	wb.log.DEBUG.Println("Read from box: ", value)
-
 	return value != 0, nil
 }
 
 // Enable implements the api.Charger interface
 func (wb *AmtronProfessional) Enable(enable bool) error {
-	wb.log.DEBUG.Println("Enable ", enable, wb.current)
 	var err error
 	if enable {
 		err = wb.MaxCurrent(int64(wb.current))
@@ -118,7 +109,6 @@ func (wb *AmtronProfessional) Enable(enable bool) error {
 
 // MaxCurrent implements the api.Charger interface
 func (wb *AmtronProfessional) MaxCurrent(current int64) error {
-	wb.log.DEBUG.Println("MaxCurrent")
 	_, err := wb.conn.WriteSingleRegister(amtronProfRegAmpsConfig, uint16(current))
 	if err == nil {
 		if current > 0 {
